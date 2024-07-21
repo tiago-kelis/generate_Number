93ea83b5cb561edf248217976cd0fbf9222d61da
Tiago kelis <tiagokelis2017@gmail.com>
2024-07-21 14:18:04 -0300

    gerar Number1

---
+++ scriptSS.js
@@ -0,0 +1,9 @@
+function sortear(){
+    const mim = document.getElementById("minimo").value;
+    const max = document.getElementById("maximo").value;
+    const sort = Math.floor((Math.random()* max) + mim);
+
+    document.getElementById("result").innerHTML = sort;
+
+
+}
